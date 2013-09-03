7de5bd7e445638afd1d2880c3919581ada39712d
ejwu <eric.j.wu@gmail.com>
2013-09-03 15:22:29 -0700

    Back to 1 thread, since it seems to be about as fast as 4 or 8 and makes benchmarking more consistent until I figure out concurrency issues. Also change the default problem to involve more branching.

--- src/GameState.java
+++ src/GameState.java
@@ -27,7 +27,7 @@ import com.google.common.collect.Multiset;
 import com.google.common.collect.Table;
 
 public class GameState {
-	private static final ExecutorService executor = Executors.newFixedThreadPool(8);
+	private static final ExecutorService executor = Executors.newFixedThreadPool(1);
 	
 	// bit mask for the full deck, used for checking that a state is valid
 	private static final long FULL_DECK_MASK;
